9224edb93132945e2f89c75408f8d3a2cb010c2d
domac.li <domacode@gmail.com>
2018-03-30 11:09:55 +0800

    add

--- rp.go
+++ rp.go
@@ -2,63 +2,129 @@ package rp
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"runtime"
 	"runtime/pprof"
+	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
+
+const DEFAULT_PORT = 10029 //默认端口
+
+type RpConfig struct {
+	DebugProfile DebugProfile `toml:"debug_profile"`
+}
+
+type DebugProfile struct {
+	ModulePorts     []int    `toml:"module_ports"`
+	ModuleNames     []string `toml:"module_names"`
+	ProfileOutpuDir string   `toml:"profile_output_dir"`
+}
+
+var g_rpconfig = new(RpConfig)
 
 type profileMux struct {
 	cpuProfile  string
 	memProfile  string
 	port        uint32
-	access      bool
+	started     uint32
 	profileTime time.Duration
 }
 
+//debug服务
 func (p *profileMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/rp" {
-		if !p.access {
+
+		if !atomic.CompareAndSwapUint32(&p.started, 0, 1) {
+			log.Printf("profile already called")
 			return
 		}
-		log.Println("get a command to create profile")
+
 		var wg sync.WaitGroup
 		wg.Add(2)
-		p.access = false
+		atomic.StoreUint32(&p.started, 1)
 		go p.ProfileCPU(p.cpuProfile, &wg)
 		go p.ProfileMEM(p.memProfile, &wg)
 		wg.Wait()
-		p.access = true
+		atomic.StoreUint32(&p.started, 0)
+		w.Header().Set("Content-type", "text/html")
+		io.WriteString(w, "profile finish\r\n")
 		return
 	}
 	http.NotFound(w, r)
 	return
 }
 
-func StartProfile(port int, cpuprofile, memprofile string, profileTime time.Duration) error {
+//默认创建快照方式
+func CreateProfile(mode int) error {
+	_, callerFileName, _, _ := runtime.Caller(1)
+	srcIndex := strings.LastIndex(callerFileName, "src")
+	if srcIndex > 0 {
+		callerFileName = callerFileName[srcIndex+1:]
+	}
+	log.Printf("caller file : %s\n", callerFileName)
 
+	port := DEFAULT_PORT
+	//获取当前的工作目录
 	wd, _ := os.Getwd()
-	if cpuprofile == "" {
+	cpuprofile := ""
+	memprofile := ""
+
+	if mode == 1 || mode == 3 {
 		cpuprofile = path.Join(wd, "debug_profile.cpu")
 	}
 
-	if memprofile == "" {
+	if mode == 2 || mode == 3 {
 		memprofile = path.Join(wd, "debug_profile.mem")
 	}
 
+	//LoadDebugProfile("/Users/lihaoquan/GoProjects/Playground/src/github.com/domac/rp/rp.conf")
+
+	if g_rpconfig != nil {
+		for idx, name := range g_rpconfig.DebugProfile.ModuleNames {
+			if strings.Contains(callerFileName, name) &&
+				len(g_rpconfig.DebugProfile.ModulePorts) > idx {
+				port = g_rpconfig.DebugProfile.ModulePorts[idx]
+
+				//如果是配置的模块名,则重新设置输出文件名称
+				if g_rpconfig.DebugProfile.ProfileOutpuDir != "" {
+
+					os.MkdirAll(g_rpconfig.DebugProfile.ProfileOutpuDir, 0777)
+
+					if cpuprofile != "" {
+						cpuprofile = path.Join(g_rpconfig.DebugProfile.ProfileOutpuDir, name+"_debug_profile_cpu.prof")
+					}
+
+					if memprofile != "" {
+						memprofile = path.Join(g_rpconfig.DebugProfile.ProfileOutpuDir, name+"_debug_profile_mem.prof")
+					}
+				}
+			}
+		}
+	}
+
+	return StartProfile(port, cpuprofile, memprofile, 30*time.Second)
+}
+
+func StartProfile(port int, cpuprofile, memprofile string, profileTime time.Duration) error {
+	//获取调用者的标准文件名称
 	mux := &profileMux{
 		cpuProfile:  cpuprofile,
 		memProfile:  memprofile,
 		port:        uint32(port),
-		access:      true,
 		profileTime: profileTime,
 	}
 
 	go func(mux *profileMux) {
 		ps := fmt.Sprintf(":%d", mux.port)
+		log.Printf("debug profile call : http://127.0.0.1%s/rp\n", ps)
 		if err := http.ListenAndServe(ps, mux); err != nil {
 			log.Fatalf("Profile  Server Failed: %v", err)
 		}
@@ -74,7 +140,6 @@ func (p *profileMux) ProfileCPU(cpuprofile string, wg *sync.WaitGroup) {
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		pprof.StartCPUProfile(f)
 		time.AfterFunc(p.profileTime, func() {
 			pprof.StopCPUProfile()
@@ -88,9 +153,7 @@ func (p *profileMux) ProfileCPU(cpuprofile string, wg *sync.WaitGroup) {
 }
 
 func (p *profileMux) ProfileMEM(memprofile string, wg *sync.WaitGroup) {
-
 	log.Printf("memory profile : %s\n", memprofile)
-
 	if memprofile != "" {
 		//检测memory profile 配置
 		f, err := os.Create(memprofile)
@@ -105,5 +168,12 @@ func (p *profileMux) ProfileMEM(memprofile string, wg *sync.WaitGroup) {
 		})
 	} else {
 		wg.Done()
+	}
+}
+
+func LoadDebugProfile(path string) {
+	_, err := toml.DecodeFile(path, &g_rpconfig)
+	if err != nil {
+		log.Println(err.Error())
 	}
 }
